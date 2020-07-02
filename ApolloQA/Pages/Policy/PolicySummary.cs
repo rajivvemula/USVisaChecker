0762c68d0cd217aed8060e7274a4df6e2deef224
Saeed Malik <Saeed.Malik@biberk.com>
2020-07-02 13:07:43 -0400

    R009-Label Check and add toaster functions

--- ApolloQA/Pages/Policy/PolicySummary.cs
+++ ApolloQA/Pages/Policy/PolicySummary.cs
@@ -28,6 +28,22 @@ namespace ApolloQA.Pages.Policy
             {"operations", "mat-tab-label-0-4" },
             {"website", "mat-tab-label-0-5" }
         };
+        
+        public readonly string[] generalInformationLabels =
+        {
+            "Policy Code",
+            "Insured Party",
+            "Agency",
+            "Carrier",
+            "Line of Business",
+            "Effective Date",
+            "Expiration Date",
+            "Business Type",
+            "NAICS",
+            "Status",
+            "Underwriter",
+            "Underwriter Agency"
+        };
 
         public IWebElement businessTab => policyDriver.FindElement(By.Id("mat-tab-label-0-0"));
         public IWebElement renewalTab => policyDriver.FindElement(By.Id("mat-tab-label-0-1"));
@@ -35,7 +51,8 @@ namespace ApolloQA.Pages.Policy
         public IWebElement accountingTab => policyDriver.FindElement(By.Id("mat-tab-label-0-3"));
         public IWebElement operationsTab => policyDriver.FindElement(By.Id("mat-tab-label-0-4"));
         public IWebElement websiteTab => policyDriver.FindElement(By.Id("mat-tab-label-0-5"));
-
+        public IWebElement selectBus => policyDriver.FindElement(By.XPath("//mat-select[@formcontrolname='businessTypeEntityId']"));
+        public IWebElement saveButton => policyDriver.FindElement(By.ClassName("save-button"));
 
         public string CheckPolicyStatus()
         {
@@ -53,5 +70,23 @@ namespace ApolloQA.Pages.Policy
             string selected = policyDriver.FindElement(By.Id(tabId[id])).GetAttribute("aria-selected");
             return selected;
         }
+
+        public bool CheckLabel(string label)
+        {
+            bool verify = policyDriver.FindElement(By.XPath("//mat-label[contains(text(),'" + label + "')]")).Displayed;
+            return verify;
+        }
+        public void EnterBusType(string corp)
+        {
+            selectBus.Click();
+            policyDriver.FindElement(By.XPath("//span[@class='mat-option-text' and normalize-space(text())='" + corp + "']")).Click();
+        }
+
+        public void ClickSaveButton()
+        {
+            saveButton.Click();
+        }
+
+
     }
 }
--- ApolloQA/Pages/Shared/Toaster.cs
+++ ApolloQA/Pages/Shared/Toaster.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +8,21 @@ namespace ApolloQA.Pages.Shared
 {
     class Toaster
     {
+
+        private IWebDriver Driver;
+        private WebDriverWait wait;
+
+        public Toaster(IWebDriver driver)
+        {
+            Driver = driver;
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(60));
+        }
+
+        public string GetToastTitle()
+        {
+            IWebElement toast = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.ClassName("toast-title")));
+            string toastTitle = toast.Text;
+            return toastTitle;
+        }
     }
 }
