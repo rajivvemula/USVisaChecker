baf263e5d9345a4ce5422129a31a8599d4380793
Casey Klaips <Casey.Klaips@biberk.com>
2020-07-10 10:01:18 -0400

    Added Functions class in Helpers directory for helper function (created wrapper for FindElement that incorporates wait and catches StaleElementReferenceException)

---
+++ ApolloQA/Helpers/Functions.cs
@@ -0,0 +1,39 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace ApolloQA.Helpers
+{
+    class Functions
+    {
+
+        private IWebDriver driver;
+
+        public Functions(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public IWebElement FindElementWait(int seconds, By by)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(seconds));
+            IWebElement target;
+
+            try
+            {
+                target = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(by));
+            }
+            catch(StaleElementReferenceException stale)
+            {
+                //retry finding the element
+                target = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(by));
+            }
+
+            return target;
+        }
+
+    }
+}
