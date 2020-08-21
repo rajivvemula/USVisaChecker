7529f4a2088ae7cc64b05ef24e47a12bb0053411
Saeed Malik <Saeed.Malik@biberk.com>
2020-08-21 10:28:35 -0400

    Setup Smoke Test Base(Removing Specflow From SmokeTests/Regression)

---
+++ ApolloQA/TestCases/Smoke Tests/SmokeTest.cs
@@ -0,0 +1,33 @@
+﻿using ApolloQA.Helpers;
+using ApolloQA.Pages.Login;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApolloQA.TestCases.Smoke_Tests
+{
+
+    [TestFixture]
+    class SmokeTest
+    {
+
+        public IWebDriver driver;
+        [SetUp]
+        public void setup()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+        }
+
+        [TestCase]
+        public void LogIn()
+        {
+            LogInAsAdmin logInAsAdmin = new LogInAsAdmin(driver);
+            logInAsAdmin.LoginAsValidUser();
+        }
+
+    }
+}
