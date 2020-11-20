5adb265c2ce3095184ecfed0824d1d2a1f3fb462
Sonia Amaravel <sonia.amaravel@biberk.com>
2020-11-20 09:35:57 -0500

    Created Print Center feature file, step definition and page object class file

---
+++ ApolloQA/Pages/Waffle/PrintCenter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ApolloQA.Source.Driver;
+using ApolloQA.Source.Helpers;
+
+namespace ApolloQA.Pages
+{
+    class PrintCenter
+    {
+
+        public static Element WaffleMenuOptionPrintCenter => new Element("//a[@href='/print-center']");
+        public static Element PrintCenterGridLastItem => new Element("(//div[@class='datatable-row-center datatable-row-group ng-star-inserted'])[last()] //*[@role='button']");
+
+    }
+}
---
+++ ApolloQA/StepDefinition/Waffle/PrintCenterSteps.cs
@@ -0,0 +1,43 @@
+﻿using TechTalk.SpecFlow;
+using ApolloQA.Source.Helpers;
+using ApolloQA.Source.Driver;
+namespace ApolloQA.Features.Waffle
+
+{
+    [Binding]
+    public class PrintCenterSteps
+    {
+        [When(@"user navigates to Printcenter Page by accessing Waffle")]
+        public void WhenUserNavigatesToPrintcenterPageByAccessingWaffle()
+        {
+           // System.Threading.Thread.Sleep(1000);
+            Pages.Home.WaffleGridButton.Click();
+
+    }
+
+    [When(@"User Clicks Print Center Link")]
+        public void WhenUserClicksPrintCenterLink()
+        {
+            Pages.PrintCenter.WaffleMenuOptionPrintCenter.Click();
+            Pages.PrintCenter.PrintCenterGridLastItem.Click();
+        }
+        
+        /*[Then(@"User should be redirected to Print Center Page")]
+        public void ThenUserShouldBeRedirectedToPrintCenterPage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"User Clicks on the Last Queue name in Print Center table")]
+        public void ThenUserClicksOnTheLastQueueNameInPrintCenterTable()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"User should see all the job associated to this Queue")]
+        public void ThenUserShouldSeeAllTheJobAssociatedToThisQueue()
+        {
+            ScenarioContext.Current.Pending();
+        }*/
+    }
+}
