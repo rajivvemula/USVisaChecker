ae0efafffa87b9dbbdef07def1140fb53fe2d4a0
Sonia Amaravel <sonia.amaravel@biberk.com>
2020-11-30 16:21:34 -0500

    PrintCenter-Update

--- ApolloQA/Pages/Waffle/PrintCenter.cs
+++ ApolloQA/Pages/Waffle/PrintCenter.cs
@@ -10,7 +10,12 @@ namespace ApolloQA.Pages
     {
 
         public static Element WaffleMenuOptionPrintCenter => new Element("//a[@href='/print-center']");
+        public static Element PrintCenterDashboard = new Element("//*[contains(text(), ' Dashboard ')]");
+        public static Element PrintCenterHeader = new Element("//*[contains(text(), 'Print Center')]");
+
         public static Element PrintCenterGridLastItem => new Element("(//div[@class='datatable-row-center datatable-row-group ng-star-inserted'])[last()] //*[@role='button']");
+
+
 
     }
 }
