11184b36cab3a4f32c8da53f898bcbf9c10dd944
Miguel Acosta <Miguel.Acosta@biberk.com>
2023-03-09 17:56:04 +0000

    Merged PR 19926: Updated FormsGenerateSteps.cs
    Updated FormsGenerateSteps.cs
    
    Related work items: #99542

--- ApolloTests/StepDefinitions/Forms/FormsGenerateSteps.cs
+++ ApolloTests/StepDefinitions/Forms/FormsGenerateSteps.cs
@@ -49,7 +49,7 @@ namespace ApolloTests.StepDefinition.Forms
             this.LineId = LineId;
             this.Form = Form.GetForm(new Line(LineId), code, name);
             bool createNewEntities = MiscHook.CurrentScenarioStatus?.outcomes?.Last()?.error??false;
-            var policy = this.Form.condition.GetValidPolicy(true);
+            var policy = this.Form.condition.GetValidPolicy(createNewEntities);
             this.Context = new FormContext(Form, policy);
             var policyId = policy.Id;
 
